4c2950033145a4125b135c43886ecd06047215af
PragyaJ23 <pragyajmera23@gmail.com>
2026-07-03 14:54:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix = strs[0]
+
+        for s in strs[1:]:
+            while s[:len(prefix)] != prefix:
+                prefix = prefix[:-1]
+
+            if prefix == "":
+                return ""
+
+        return prefix
+strs=["flower","flow","flight"]
+s=Solution()
+s.longestCommonPrefix(strs)
+
+        
